21fd10de15dc8ac946c89ab8331ae2cca6576d3a
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-12-26 21:24:15 -0600

    Renderiing template.

---
+++ girasol/girasol/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'index.html', {
+        'message': 'Listado de productos',
+        'title': 'Productos',
+        'products': [
+            {'title': 'Playera', 'price': 5, 'stock': True},
+            {'title': 'Camisa', 'price': 7, 'stock': True},
+            {'title': 'Mochila', 'price': 20, 'stock': False},
+            {'title': 'Laptop', 'price': 500, 'stock': False},
+        ]
+    })
